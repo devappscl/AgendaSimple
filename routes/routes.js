1dcf5d59be002e8a2f0bdf21211c71776de3d9fc
devappscl <jonathan.hernandez1@virginiogomez.cl>
2023-01-30 14:32:30 -0300

    Routes modified

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const router = express.Router();
-const ContactosController = require('../controller/contactos')
+const ContactosController = require('../controllers/contactos')
 
 let api_ruta = "/api/v1/"
 
